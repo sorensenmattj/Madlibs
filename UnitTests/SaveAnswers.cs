f48f6141dd28a57795a2d55adafbf786c2368025
Matt Sorensen <sorensenmattj@gmail.com>
2018-11-02 17:57:20 +0000

    New branch created.

---
+++ UnitTests/SaveAnswers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MadLibs.MadLibsComponents;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void KeepProvidedAnswers()
+        {
+            var madLibsMenu = new MadLibsMenu();
+
+            var madLib = madLibsMenu.GetMadLib(Console.Out, new StringReader("1"));
+        }
+    }
+}
